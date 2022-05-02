51e67692e74598b9fee61363dd10d91d02304b34
Fernando Costa <fernandocostadev98@gmail.com>
2022-05-02 09:05:26 -0300

    Singleton repository

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
+
+class ListCategoriesController {
+  constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
+  handle(req: Request, res: Response) {
+    const all = this.listCategoriesUseCase.execute();
+
+    return res.status(201).json(all);
+  }
+}
+
+export { ListCategoriesController };
